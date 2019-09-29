1fbf0b9d078810f194fd7fdc62acb2d1b85ba18a
Pedro <30598480+TricksterByte@users.noreply.github.com>
2019-09-29 12:53:31 +0100

    added updated lab scripts

---
+++ lab1/DynamicArrive.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicArrive : DynamicVelocityMatch
+    {
+
+        public float MaxSpeed   { get; set; }
+        public float StopRadius { get; set; }
+        public float SlowRadius { get; set; }
+
+        public KinematicData DesiredTarget { get; set; } 
+
+        public DynamicArrive()
+        {
+            this.MaxSpeed   = 40.0f;
+            this.StopRadius = 10.0f;
+            this.SlowRadius = 40.0f;
+            this.Output     = new MovementOutput();
+        }
+
+        public override MovementOutput GetMovement()
+        {
+            base.Target = new KinematicData(this.DesiredTarget, this.DesiredTarget.velocity);
+
+            Vector3 direction = this.DesiredTarget.Position - this.Character.Position;
+            float distance    = direction.magnitude;
+            float desiredSpeed;
+
+            if (distance < this.StopRadius) desiredSpeed = 0;
+            else if (distance > this.SlowRadius) desiredSpeed = this.MaxSpeed;
+            else desiredSpeed = this.MaxSpeed * (distance / this.SlowRadius);
+
+            base.Target.velocity = direction.normalized * desiredSpeed;
+
+            return base.GetMovement();
+        }
+    }
+}
---
+++ lab1/DynamicWander.cs
@@ -0,0 +1,50 @@
+﻿using Assets.Scripts.IAJ.Unity.Util;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+
+    public class DynamicWander : DynamicSeek
+    {
+        public float WanderOffset { get; set; }
+        public float WanderRadius { get; set; }
+        public float WanderRate { get; set; }
+
+        public float WanderAngle { get; protected set; }
+
+        public Vector3 CircleCenter { get; private set; }
+
+        public GameObject DebugTarget { get; set; }
+
+        public DynamicWander()
+        {
+            this.Target = new KinematicData();
+            this.WanderAngle = 0;
+            this.WanderRadius = 5.0f;
+        }
+
+        public override string Name
+        {
+            get { return "Wander"; }
+        }
+
+
+        public override MovementOutput GetMovement()
+        {
+            this.WanderAngle += RandomHelper.RandomBinomial() * this.WanderRate;
+
+            float TargetOrientation = this.WanderAngle + this.Character.Orientation;
+
+            CircleCenter = this.Character.Position + WanderOffset * Character.GetOrientationAsVector();
+
+            base.Target.Position = CircleCenter + WanderRadius * MathHelper.ConvertOrientationToVector(TargetOrientation);
+
+            if (this.DebugTarget != null)
+            {
+                this.DebugTarget.transform.position = this.Target.Position;
+                
+            }
+            return base.GetMovement();
+        }
+    }
+}
