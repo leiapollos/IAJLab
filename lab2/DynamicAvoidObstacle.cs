48a9adada204b7aef64105cf9fbf1eadbf0d5dae
Pedro <30598480+TricksterByte@users.noreply.github.com>
2019-10-02 18:09:59 +0100

    added obstacle avoidance files

---
+++ lab2/DynamicAvoidObstacle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicAvoidObstacle : DynamicSeek
+    {
+
+        public float MaxLookAhead { get; set; }
+
+        public float AvoidMargin { get; set; }
+
+        public Collider Collider { get; set; }
+
+        public DynamicAvoidObstacle(GameObject obstacle) {
+            this.Collider = obstacle.GetComponent<Collider>();
+        }
+
+        public override MovementOutput GetMovement()
+        {
+            Vector3 rayVector = this.Character.Position.normalized;
+            RaycastHit info;
+
+            bool collision = this.Collider.Raycast(new Ray(this.Character.Position, rayVector), out info, this.MaxLookAhead);
+
+            if (!collision)
+                return new MovementOutput();
+
+            base.Target.Position = info.point + info.normal * this.AvoidMargin;
+
+            return base.GetMovement();
+        }
+    }
+}
