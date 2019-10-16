e919fa36c583370caef35d39895230dafc0944aa
leiapollos <andreleite98@hotmail.com>
2019-10-16 18:13:00 +0100

    lab3

---
+++ lab3/EuclideanHeuristic.cs
@@ -0,0 +1,12 @@
+﻿using RAIN.Navigation.Graph;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics
+{
+    public class EuclideanHeuristic : IHeuristic
+    {
+        public float H(NavigationGraphNode node, NavigationGraphNode goalNode)
+        {
+            return (goalNode.Position-node.Position).sqrMagnitude;
+        }
+    }
+}
