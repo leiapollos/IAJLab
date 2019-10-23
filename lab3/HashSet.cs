82781064bf1c46ce6df65f6d986dd9f51e103e53
Pedro <30598480+TricksterByte@users.noreply.github.com>
2019-10-23 18:36:35 +0100

    hash set and node array implementation

---
+++ lab3/HashSet.cs
@@ -0,0 +1,42 @@
+﻿using RAIN.Navigation.Graph;
+using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures
+{
+    public class HashSet : IClosedSet
+    {
+        private IDictionary<NavigationGraphNode, NodeRecord> NodeRecords { get; set; }
+
+        public HashSet()
+        {
+            this.NodeRecords = new Dictionary<NavigationGraphNode, NodeRecord>();
+        }
+        public void Initialize()
+        {
+            this.NodeRecords.Clear();
+        }
+
+        public void AddToClosed(NodeRecord nodeRecord)
+        {
+            this.NodeRecords.Add(nodeRecord.node, nodeRecord);
+        }
+
+        public void RemoveFromClosed(NodeRecord nodeRecord)
+        {
+            this.NodeRecords.Remove(nodeRecord.node);
+        }
+
+        public NodeRecord SearchInClosed(NodeRecord nodeRecord)
+        {
+            if (this.NodeRecords.ContainsKey(nodeRecord.node))
+                return this.NodeRecords[nodeRecord.node];
+            return null;
+        }
+
+        public ICollection<NodeRecord> All()
+        {
+            return new List<NodeRecord>(this.NodeRecords.Values);
+        }
+    }
+}
